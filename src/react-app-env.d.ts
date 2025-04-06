62ee564e0e69007c53244a256c4c2fd12e9b0c86
Renzo Dogliotti <renzodogliotti@gmail.com>
2025-04-06 16:51:23 -0300

    va

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,4 +1,1 @@
-declare module 'materialize-css' {
-    const M: any; // Usamos 'any' para simplicidad, pero podrías tiparlo más específicamente si querés
-    export default M;
-  }
+/// <reference types="react-scripts" />
