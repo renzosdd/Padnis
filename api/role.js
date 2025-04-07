b77f32ed807c07bb095582ab3252760950ff0066
Renzo Dogliotti <renzodogliotti@gmail.com>
2025-04-07 17:50:10 -0300

    Update role.js

--- api/role.js
+++ api/role.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = require('../models/User');
-
 mongoose.connect(process.env.MONGO_URI);
 
 const authenticateToken = (req) => {
