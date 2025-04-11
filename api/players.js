edeae14572aafed9f1620cdaa25d9448b9efb9ba
Renzo Dogliotti <renzodogliotti@gmail.com>
2025-04-11 11:54:03 -0300

    JWT

--- api/players.js
+++ api/players.js
@@ -8,7 +8,7 @@ const authenticateToken = (req) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   if (!token) throw new Error('Acceso denegado');
-  return jwt.verify(token, process.env.JWT_SECRET || 'secret_key');
+  return jwt.verify(token, process.env.JWT_SECRET);
 };
 
 module.exports = async (req, res) => {
