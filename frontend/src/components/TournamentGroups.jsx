73e0cad61b96c4eeac8864f3e7906c6a8a77734b
Renzo Dogliotti <renzodogliotti@gmail.com>
2025-04-28 00:13:55 -0300

    Update TournamentGroups.jsx

--- frontend/src/components/TournamentGroups.jsx
+++ frontend/src/components/TournamentGroups.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Box, Typography } from '@mui/material'; // Agrega Box aquí
 import MatchCard from './MatchCard';
 
 const TournamentGroups = ({
